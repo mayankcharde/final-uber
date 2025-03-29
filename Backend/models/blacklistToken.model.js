f9f747909caa42569d1f47ed5ef3b883a672aa6e
mayankcharde <mayankcharde90@gmail.com>
2025-03-29 20:16:21 +0530

    black

--- Backend/models/blacklistToken.model.js
+++ Backend/models/blacklistToken.model.js
@@ -13,4 +13,4 @@ const blackListTokenSchema = new mongoose.Schema({
     }
 });
 
-module.exports = mongoose.model('BlackListToken', blackListTokenSchema);
+module.exports = mongoose.model('BlackListToken', blackListTokenSchema); 
